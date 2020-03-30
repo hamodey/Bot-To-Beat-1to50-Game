79eeb97cc5c4f52cafbf061aa045641757f48763
Ahmed Al-waili <aalw001@gold.ac.uk>
2020-03-30 16:22:06 +0100

    the bot

---
+++ the_bot.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+import time
+
+class GameBot:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("http://zzzscore.com/1to50/en/?ts=1585578322745")
+        time.sleep(5)
+
+        elem = self.driver.find_element_by_id("grid")
+        # child_elem = elem.find_element_by_xpath("//div[text() = '1']")
+        # print(child_elem.text)
+        # child_elem.click()
+        self.runGame(elem)
+        time.sleep(10)
+
+    def runGame(self, parent):
+        for i in range(1, 51):
+            #elem = self.driver.find_element_by_id("grid")
+            
+            child_elem = parent.find_element_by_xpath('//div[text() = "'+ str(i) + '"]')
+            print(child_elem.text)
+            child_elem.click()
+        
+            # elem = parent.find_element_by_xpath("//span[contains(text(), '1')]")
+            # print(elem.text)
+GameBot()
